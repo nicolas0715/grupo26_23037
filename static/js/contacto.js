ebde4022fb8c12e0c5df4f068ea3a5ab09af8646
Otero <oteronicolas3@gmail.com>
2023-05-21 21:27:54 -0300

    Agregue la api del clima que me mando hernan, pero no me funciono. Igualmente agregue la api del dolar. Tambien agregue un efecto en las imagenes en la pagina de AcercaDe. Y termine la pagina de CityTour. La de contacto la modifique, a ver si les gusta. Y faltan todavia las imagenes en la pagina de paquetes y quizas algun que otro contenido mas a la pagina de index

--- static/js/contacto.js
+++ static/js/contacto.js
@@ -9,3 +9,44 @@ function confirmacion() {
     
     alert('Estos son los datos ingresados: '+ nombre + apellido + telefono + email+ comentario  );
 }
+
+var imagenes = [
+    './static/images/santa-rosa-tastil.jpg',
+    './static/images/seclanta.jpg', 
+    './static/images/cachi3.jpg', 
+    './static/images/campo_quijano.jpg'
+];
+var indice = 0;
+
+function cambiarImagen() {
+    var imagen = document.getElementById('imagenes-varias');
+    indice++;
+    if (indice >= imagenes.length) {
+        indice = 0;
+    }
+    imagen.src = imagenes[indice];
+}
+
+
+function api(){
+    tempCont=document.querySelector('#temp_api')
+    fetch('https://api.open-meteo.com/v1/forecast?latitude=-24.79&longitude=-65.41&hourly=temperature_2m')
+    .then(res=>res.json())
+    .then(data=>{
+        tempCont.innerHTML=data.hourly.temperature_2m[0]
+        console.log(data.hourly.temperature_2m[0])
+    }
+        )
+}
+
+
+
+fetch('https://www.dolarsi.com/api/api.php?type=valoresprincipales') 
+        .then(response => response.json())
+        .then(data => {
+            const dolarOficial = data.find(item => item.casa.nombre === 'Dolar Oficial');
+            const dolarBlue = data.find(item => item.casa.nombre === 'Dolar Blue');
+
+            document.getElementById('moneda1').textContent = dolarOficial.casa.compra;
+            document.getElementById('moneda2').textContent = dolarBlue.casa.compra;
+        });
